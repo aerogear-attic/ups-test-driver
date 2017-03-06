ddcd324b1aeed3e05e3868fb402413f90cec905d
Jose Miguel Gallas Olmedo <josemigallas@gmail.com>
2017-03-06 10:00:54 +0100

    :alien: updates csv-helper

--- app/util/csv-helper.js
+++ app/util/csv-helper.js
@@ -3,8 +3,6 @@
 const csv = require("fast-csv");
 const fs = require("fs");
 
-const COLUMN_NAME_ALIAS = "TOKEN ALIAS";
-
 class CSVHelper {
 
     constructor(csvPath) {
@@ -15,15 +13,13 @@ class CSVHelper {
         return new Promise((resolve, reject) => {
             const stream = fs.createReadStream(this.csvPath);
             const options = {
-                // headers: ["variantId", "alias", "token"],
-                // renameHeaders: true
-                headers: true
+                headers: ["variantId", "alias", "token"],
+                renameHeaders: true
             };
             const aliases = [];
             const csvStream = csv
                 .parse(options)
-                // .on("data", row => aliases.push(row.alias))
-                .on("data", row => aliases.push(row[COLUMN_NAME_ALIAS]))
+                .on("data", row => aliases.push(row.alias))
                 .on("error", err => reject(err))
                 .on("end", () => resolve(aliases));
 
